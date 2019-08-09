f117f4faa75dca5fa109bac066adb3b3ee5c6cf7
Riley Shaw <rileyjshaw@gmail.com>
2019-08-09 14:25:25 -0700

    Firehose: proof of concept
    I'm experimenting with auto-generating nodes for
    https://rileyjshaw.com/lab from a variety of data sources. This
    project may eventually replace https://rileyjshaw.com.
    
    This is the initial commit, completed quickly as a proof of concept.
    There's nothing much to show, but I want to deploy ASAP so I can test
    the full pipeline.
    
    So far, everything has worked! Data from a variety of sources is
    already appearing on my local server. To reproduce:
    
    - Run `gatsby develop`.
    - Open http://localhost:8000/page-2 in a browser.
    
    So far, I'm surfacing data from:
    
    - A hand-curated JSON list of projects I use for
      https://rileyjshaw.com/lab.
    - [Dwitter](https://dwitter.net)'s API.
    - [My commit--blog](https://rileyjshaw.commit--blog.com)'s Atom feed.
    
    Setting this up was *EASY*, which makes me excited for the future of
    this experiment :)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,71 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-}
+	siteMetadata: {
+		title: `rileyjshaw`,
+		description: `gatsby new gatsby-site`,
+		author: `@rileyjshaw`,
+	},
+	plugins: [
+		`gatsby-plugin-react-helmet`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/images`,
+			},
+		},
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `gatsby-starter-default`,
+				short_name: `starter`,
+				start_url: `/`,
+				background_color: `#663399`,
+				theme_color: `#663399`,
+				display: `minimal-ui`,
+				icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+			},
+		},
+		`gatsby-transformer-json`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				path: `${__dirname}/src/data/`,
+			},
+		},
+		{
+			resolve: `gatsby-source-atom`,
+			options: {
+				source: `http://rileyjshaw.commit--blog.com/feed`,
+			},
+		},
+		// HACK(riley): Can I get this source to dynamically use the "next" key?
+		...Array.from({length: 5}, (_, i) => ({
+			resolve: `gatsby-source-custom-api`,
+			options: {
+				url: `https://www.dwitter.net/api/dweets/?author=rileyjshaw&limit=10&offset=${i *
+					10}`,
+				rootKey: `dweets${i}`,
+				schemas: {
+					dweets: `
+						next: String
+						results: results
+					`,
+					results: `
+						id: Number
+						link: String
+						author: author
+					`,
+					author: `
+						username: String
+					`,
+				},
+			},
+		})),
+		// TODO(riley)
+		// this (optional) plugin enables Progressive Web App + Offline functionality
+		// To learn more, visit: https://gatsby.dev/offline
+		// `gatsby-plugin-offline`,
+	],
+};
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,99 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import {Link, StaticQuery, graphql} from 'gatsby';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const labsList = () => (
+	<StaticQuery
+		query={graphql`
+			query ProjectsQuery {
+				allProjectsJson {
+					nodes {
+						title
+						description
+					}
+				}
+				allDweetsJson {
+					nodes {
+						id
+					}
+				}
+				allAtomEntry {
+					nodes {
+						title
+						date(formatString: "YYYY-MM-DD")
+						link
+					}
+				}
+				dweets0 {
+					results {
+						id__normalized
+						link
+					}
+				}
+				dweets1 {
+					results {
+						id__normalized
+						link
+					}
+				}
+			}
+		`}
+		render={({
+			allAtomEntry: {nodes: commits},
+			allDweetsJson: {nodes: dweets},
+			allProjectsJson: {nodes: projects},
+			dweets0: {results: d0},
+			dweets1: {results: d1},
+		}) => (
+			<ul>
+				{[...d0, ...d1].map(({id__normalized, link}) => (
+					<li>
+						<a href={link}>Dweet {id__normalized}</a>
+					</li>
+				))}
+				{commits.map(({title, date, link}) => (
+					<li>
+						<a href={link}>
+							<h1>
+								{title} ({date})
+							</h1>
+						</a>
+					</li>
+				))}
+				{dweets.map(({id}) => (
+					<li key={id}>
+						<h2>Dweet {id}</h2>
+					</li>
+				))}
+				{projects
+					.filter(({todo}) => !todo)
+					.map(({title, description}) => (
+						<li key={title}>
+							<h2>{title}</h2>
+							{description &&
+								description.map(paragraph => (
+									<p
+										dangerouslySetInnerHTML={{
+											__html: paragraph,
+										}}
+									/>
+								))}
+						</li>
+					))}
+			</ul>
+		)}
+	/>
+);
 
 const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+	<Layout>
+		<SEO title="Page two" />
+		<h1>The firehose!</h1>
+		{labsList()}
+		<Link to="/">Go back to the homepage</Link>
+	</Layout>
+);
 
-export default SecondPage
+export default SecondPage;
