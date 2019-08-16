8b7e2daac963f95a04cb871c7e9ff12874c19c81
Riley Shaw <rileyjshaw@gmail.com>
2019-08-15 17:49:06 -0700

    Add reusable layout and header components

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,44 @@
-import {Link} from 'gatsby';
-import PropTypes from 'prop-types';
 import React from 'react';
+import {Link, StaticQuery, graphql} from 'gatsby';
 
-const Header = ({siteTitle}) => (
-	<header
-		style={{
-			background: `rebeccapurple`,
-			marginBottom: `1.45rem`,
-		}}
-	>
-		<div
-			style={{
-				margin: `0 auto`,
-				maxWidth: 960,
-				padding: `1.45rem 1.0875rem`,
-			}}
-		>
-			<h1 style={{margin: 0}}>
-				<Link
-					to="/"
-					style={{
-						color: `white`,
-						textDecoration: `none`,
-					}}
-				>
-					{siteTitle}
-				</Link>
-			</h1>
-		</div>
-	</header>
+import './header.css';
+
+const Header = () => (
+	<StaticQuery
+		query={graphql`
+			{
+				site {
+					siteMetadata {
+						title
+					}
+				}
+			}
+		`}
+		render={({site}) => (
+			<header className="page-header">
+				<h1 className="home-link">
+					<Link to="/">{site.siteMetadata.title}</Link>
+				</h1>
+				<ul>
+					<li>
+						<Link to="/">/ home</Link>
+					</li>
+					<li>
+						<Link to="/about">/ about</Link>
+					</li>
+					<li>
+						<Link to="/blog">/ blog</Link>
+					</li>
+					<li>
+						<Link to="/firehose">/ firehose</Link>
+					</li>
+					<li>
+						<Link to="/subscribe">/ subscribe</Link>
+					</li>
+				</ul>
+			</header>
+		)}
+	/>
 );
-
-Header.propTypes = {
-	siteTitle: PropTypes.string,
-};
-
-Header.defaultProps = {
-	siteTitle: ``,
-};
 
 export default Header;
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,56 +1,60 @@
 import React from 'react';
 import {StaticQuery, graphql} from 'gatsby';
 
+import Layout from '../components/layout';
+
 import './about.css';
 
 export default () => (
-	<StaticQuery
-		query={graphql`
-			{
-				me: file(relativePath: {eq: "about/me-full.md"}) {
-					childMarkdownRemark {
-						html
+	<Layout>
+		<StaticQuery
+			query={graphql`
+				{
+					me: file(relativePath: {eq: "about/me-full.md"}) {
+						childMarkdownRemark {
+							html
+						}
+					}
+					site: file(relativePath: {eq: "about/site.md"}) {
+						childMarkdownRemark {
+							html
+						}
 					}
 				}
-				site: file(relativePath: {eq: "about/site.md"}) {
-					childMarkdownRemark {
-						html
-					}
-				}
-			}
-		`}
-		render={({me, site}) => (
-			<>
-				<blockquote cite="https://www.versobooks.com/books/3002-new-dark-age">
-					<p>
-						If we do not understand how complex technologies
-						function, how systems of technologies interconnect, and
-						how systems of systems interact, then we are powerless
-						within them, and their potential is more easily
-						captured by selfish elites and inhuman corporations…
-						this understanding cannot be limited to the
-						practicalities of how things work: it must be extended
-						to how things came to be, and how they continue to
-						function in the world in ways that are often invisible
-						and interwoven. What is required is not understanding,
-						but literacy.
-					</p>
-					<footer>
-						—James Bridle, <cite>New Dark Age</cite>
-					</footer>
-					<p>Scroll down arrow goes here!</p>
-				</blockquote>
-				<div
-					dangerouslySetInnerHTML={{
-						__html: me.childMarkdownRemark.html,
-					}}
-				/>
-				<div
-					dangerouslySetInnerHTML={{
-						__html: site.childMarkdownRemark.html,
-					}}
-				/>
-			</>
-		)}
-	/>
+			`}
+			render={({me, site}) => (
+				<>
+					<blockquote cite="https://www.versobooks.com/books/3002-new-dark-age">
+						<p>
+							If we do not understand how complex technologies
+							function, how systems of technologies interconnect,
+							and how systems of systems interact, then we are
+							powerless within them, and their potential is more
+							easily captured by selfish elites and inhuman
+							corporations… this understanding cannot be limited
+							to the practicalities of how things work: it must
+							be extended to how things came to be, and how they
+							continue to function in the world in ways that are
+							often invisible and interwoven. What is required is
+							not understanding, but literacy.
+						</p>
+						<footer>
+							—James Bridle, <cite>New Dark Age</cite>
+						</footer>
+						<p>Scroll down arrow goes here!</p>
+					</blockquote>
+					<div
+						dangerouslySetInnerHTML={{
+							__html: me.childMarkdownRemark.html,
+						}}
+					/>
+					<div
+						dangerouslySetInnerHTML={{
+							__html: site.childMarkdownRemark.html,
+						}}
+					/>
+				</>
+			)}
+		/>
+	</Layout>
 );
