c6fe9596c110324832c3e7989fe1477dcd9d09c1
Riley Shaw <rileyjshaw@gmail.com>
2019-09-01 00:42:05 -0600

    Don't hide <Fit> with CSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link, useStaticQuery, graphql} from 'gatsby';
 
 import {colors} from '../util/constants';
+import {useWindowSize} from '../util/hooks';
 import allProjectsQuery from '../util/all-projects-query';
 import SEO from '../components/seo';
 import Fit from '../components/fit';
@@ -25,6 +26,8 @@ const IndexPage = ({starredProjects = []}) => {
 		}
 	`);
 
+	const [windowWidth] = useWindowSize();
+
 	return (
 		<Layout root={true} noHeader={true}>
 			<SEO title="Home" />
@@ -33,10 +36,24 @@ const IndexPage = ({starredProjects = []}) => {
 					<div className="todo-maybe-header-element">
 						<StretchTitle>
 							<Fit>Welcome to the digital landfill of</Fit>
-							<Fit className="title-riley">Riley</Fit>
-							<p className="title-j">J</p>
-							<Fit className="title-shaw">Shaw</Fit>
-							<Fit className="title-rileyjshaw">rileyjshaw</Fit>
+							{/* Keep this up to date with the media query. */}
+							{windowWidth > 800 ? (
+								[
+									<Fit className="title-riley" key="r">
+										Riley
+									</Fit>,
+									<p className="title-j" key="j">
+										J
+									</p>,
+									<Fit className="title-shaw" key="s">
+										Shaw
+									</Fit>,
+								]
+							) : (
+								<Fit className="title-rileyjshaw">
+									rileyjshaw
+								</Fit>
+							)}
 						</StretchTitle>
 						<div className="about-stub">
 							<div
