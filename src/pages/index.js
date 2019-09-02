4da746f4eaaa90064094b174bdc4218f530071a9
Riley Shaw <rileyjshaw@gmail.com>
2019-09-01 20:53:29 -0700

    Add pretty placeholders to the .currently section until I cross that bridge

--- src/pages/index.js
+++ src/pages/index.js
@@ -80,21 +80,24 @@ const IndexPage = ({starredProjects = []}) => {
 						<h2>What I'm up to</h2>
 						<ul className="currently">
 							<li>
-								I'm redoing my website. This is a work in
-								progress, big time.
+								{/* I'm redoing my website. This is a work in
+								progress, big time. */}
+								1
 							</li>
 							<li>
-								I'm creating a massive MIDI controller for
-								music and games.
+								{/* I'm creating a massive MIDI controller for
+								music and games. */}
+								2
 							</li>
 							<li>
-								I'm working with Canada Learning Code as an
+								{/* I'm working with Canada Learning Code as an
 								instructor and mentor. My next class is{' '}
 								<ExternalLink to="https://www.canadalearningcode.ca/experiences/vancouver-chapter-girls-learning-code-gamemaking-and-circuitry-with-scratch-makey-makey/">
 									Gamemaking and Circuitry with Scratch &
 									MaKey MaKey
 								</ExternalLink>
-								.
+								. */}
+								3
 							</li>
 						</ul>
 						<h2>
