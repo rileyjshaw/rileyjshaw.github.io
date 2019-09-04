a95d7ab550d147561a4be5ee2eb80b6d294064f6
Riley Shaw <rileyjshaw@gmail.com>
2019-09-03 17:15:36 -0700

    Delete Gatsby images directory

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
 			resolve: `gatsby-source-filesystem`,
 			options: {
 				name: `images`,
-				path: `${__dirname}/src/images`,
+				path: `${__dirname}/content/images`,
 			},
 		},
 		`gatsby-transformer-sharp`,
@@ -18,13 +18,14 @@ module.exports = {
 		{
 			resolve: `gatsby-plugin-manifest`,
 			options: {
-				name: `gatsby-starter-default`,
-				short_name: `starter`,
+				name: `The digital landfill of rileyjshaw`,
+				short_name: `rileyjshaw`,
 				start_url: `/`,
-				background_color: `#663399`,
-				theme_color: `#663399`,
+				background_color: `#0074d9`,
+				theme_color: `#0074d9`,
 				display: `minimal-ui`,
-				icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+				// TODO(riley): Favicon.
+				// icon: `content/images/favicon.png`,
 			},
 		},
 		{
