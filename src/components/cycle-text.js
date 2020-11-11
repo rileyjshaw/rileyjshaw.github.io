f4b49d7bf0cb14fc68310699af291a4c7b3e0f79
Riley Shaw <rileyjshaw@gmail.com>
2020-11-10 17:59:50 -0800

    Chore: run scraper and linter

--- src/components/cycle-text.js
+++ src/components/cycle-text.js
@@ -32,49 +32,54 @@ const mergeRefs = (...refs) => {
 	};
 };
 
-export default React.forwardRef(({
-	children: text,
-	ms = 300,
-	size = [1, 1],
-	classPrefix = 'cycle-text',
-	className = '',
-	OuterElement = 'div',
-	...rest
-}, passedRef) => {
-	const [xSize, ySize] = size;
-	const n = xSize * ySize;
-	const [index, setIndex] = useState(0);
-	const [ref, inView] = useInView({threshold: 0});
-	useInterval(() => setIndex(index + 1), inView ? ms : null);
+export default React.forwardRef(
+	(
+		{
+			children: text,
+			ms = 300,
+			size = [1, 1],
+			classPrefix = 'cycle-text',
+			className = '',
+			OuterElement = 'div',
+			...rest
+		},
+		passedRef
+	) => {
+		const [xSize, ySize] = size;
+		const n = xSize * ySize;
+		const [index, setIndex] = useState(0);
+		const [ref, inView] = useInView({threshold: 0});
+		useInterval(() => setIndex(index + 1), inView ? ms : null);
 
-	if (n === 1)
+		if (n === 1)
+			return (
+				<OuterElement
+					ref={mergeRefs(passedRef, ref)}
+					className={`${className} ${classPrefix}-item`}
+					{...rest}
+				>
+					{text[index % text.length]}
+				</OuterElement>
+			);
+
 		return (
-			<OuterElement
+			<TextGrid
+				size={size}
+				classPrefix={classPrefix}
+				className={className}
 				ref={mergeRefs(passedRef, ref)}
-				className={`${className} ${classPrefix}-item`}
+				OuterElement={OuterElement}
 				{...rest}
 			>
-				{text[index % text.length]}
-			</OuterElement>
+				{Array.from(
+					{length: n},
+					(_, i) =>
+						text[
+							Math.floor(index / Math.pow(text.length, i)) %
+								text.length
+						]
+				).join('')}
+			</TextGrid>
 		);
-
-	return (
-		<TextGrid
-			size={size}
-			classPrefix={classPrefix}
-			className={className}
-			ref={mergeRefs(passedRef, ref)}
-			OuterElement={OuterElement}
-			{...rest}
-		>
-			{Array.from(
-				{length: n},
-				(_, i) =>
-					text[
-						Math.floor(index / Math.pow(text.length, i)) %
-							text.length
-					]
-			).join('')}
-		</TextGrid>
-	);
-});
+	}
+);
