083740938961f3b8b2df3d959d213f91347dfd53
Riley Shaw <rileyjshaw@gmail.com>
2020-11-18 20:43:56 -0800

    Design: add a BlogBanner component for updates, edits, notes

---
+++ src/components/blog-banner.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './blog-banner.css';
+
+const bannerTypes = {
+	edit: {
+		title: 'Edit',
+	},
+	note: {
+		title: 'Note',
+	},
+	update: {
+		title: 'Update',
+	},
+};
+
+export default ({children, date, type = 'update'}) => (
+	<div className={`blog-banner blog-banner-${type}`}>
+		<div className="blog-banner-title">
+			{`${bannerTypes[type].title}${date ? ` ${date}` : ''}:`}
+		</div>
+		<div className="blog-banner-content">
+			{children}
+		</div>
+	</div>
+);
