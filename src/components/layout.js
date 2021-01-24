f487882fa57c82b2b7ce09958d4e4b17391dd09b
Riley Shaw <rileyjshaw@gmail.com>
2021-01-23 20:13:39 -0800

    Design: add a linear backoff to the onIdle blocker component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,28 @@
 import React, {useState} from 'react';
 
-import {useIdle} from '../util/hooks';
+import {useIdle, useStickyState} from '../util/hooks';
 import Blocker from './blocker';
 
 import './layout.css';
 
 const Layout = ({children}) => {
 	const [isBlockerOpen, setIsBlockerOpen] = useState(false);
-	useIdle(60000 * 4, () => setIsBlockerOpen(true));
+	const [nTimesClosed, setNTimesClosed] = useStickyState(
+		0,
+		'nTimesClosedBlocker',
+		'session'
+	);
+	useIdle(60000 * 4 * (nTimesClosed + 1), () => setIsBlockerOpen(true));
 
 	return (
 		<>
 			{isBlockerOpen && (
-				<Blocker onClose={() => setIsBlockerOpen(false)} />
+				<Blocker
+					onClose={() => {
+						setIsBlockerOpen(false);
+						setNTimesClosed(n => n + 1);
+					}}
+				/>
 			)}
 			<div className="site-content">{children}</div>
 		</>
