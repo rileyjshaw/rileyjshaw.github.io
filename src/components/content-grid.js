b39f8c590f6ccbdd872794cee1586936deb6ec55
Riley Shaw <rileyjshaw@gmail.com>
2021-02-06 20:26:30 -0800

    Refactor: update project-explorer to a functional component
    There goes the last Class Component on this website! This change is a
    huge plus for consistency (eg. the page can now use hooks like
    useStickyState), it opens up possibilities for better linter/prettier
    options, and it fixes a few performance issues on /lab along the way.
    Good change!

--- src/components/content-grid.js
+++ src/components/content-grid.js
@@ -4,15 +4,17 @@ import ContentNode from './content-node';
 
 import './content-grid.css';
 
-export default React.forwardRef(({nodes, masonry}, ref) => (
-	<ul className={`content-grid${masonry ? ' masonry' : ''}`}>
-		{nodes.map((node, i, {length}) => (
-			<ContentNode
-				masonry={masonry}
-				ref={i === length - 1 ? ref : null}
-				key={node.uid}
-				{...node}
-			/>
-		))}
-	</ul>
-));
+export default React.memo(
+	React.forwardRef(({nodes, masonry}, ref) => (
+		<ul className={`content-grid${masonry ? ' masonry' : ''}`}>
+			{nodes.map((node, i, {length}) => (
+				<ContentNode
+					masonry={masonry}
+					ref={i === length - 1 ? ref : null}
+					key={node.uid}
+					{...node}
+				/>
+			))}
+		</ul>
+	))
+);
