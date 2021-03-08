b953e83b291e449133c6a51b2eb503fb174ac547
Riley Shaw <rileyjshaw@gmail.com>
2021-03-08 11:04:48 -0800

    Bugfix: fix 404 to have dynamic pathname (updates on client)

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,56 +3,61 @@ import {Time} from '../components/blocker';
 import SEO from '../components/seo';
 import './404.css';
 import {Link} from 'gatsby';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
-// TODO: Update design, check `location` with a useEffect.
-const NotFoundPage = ({location}) => (
-	<>
-		<SEO title="404: Not found" className="page-not-found" />
-		<main className="404-main">
-			<p>
-				You tried to load a page that doesn't exist. Here are some
-				things you can do:
-			</p>
-			<ul>
-				<li>
-					<p>
-						Start over from <Link to="/">the home page</Link>.
-					</p>
-				</li>
-				<li>
-					<p>
-						Check if the link works on my{' '}
-						<ExternalLink
-							to={`https://v14.rileyjshaw.com${location.pathname}`}
-						>
-							old website
-						</ExternalLink>
-						.
-					</p>
-				</li>
-				<li>
-					<p>
-						Search for{' '}
-						<ExternalLink
-							to={`https://duckduckgo.com/?q=rileyjshaw${location.pathname.replace(
-								/[/ _-]/g,
-								'+'
-							)}`}
-						>
-							related keywords online
-						</ExternalLink>
-						.
-					</p>
-				</li>
-				<li>
-					<p>
-						Get off the Internet. It's already <Time />.
-					</p>
-				</li>
-			</ul>
-		</main>
-	</>
-);
+function NotFoundPage({location}) {
+	const [pathname, setPathname] = useState('');
+	useEffect(() => {
+		setPathname(location.pathname);
+	}, [location]);
+	return (
+		<>
+			<SEO title="404: Not found" className="page-not-found" />
+			<main className="404-main">
+				<p>
+					You tried to load a page that doesn't exist. Here are some
+					things you can do:
+				</p>
+				<ul>
+					<li>
+						<p>
+							Start over from <Link to="/">the home page</Link>.
+						</p>
+					</li>
+					<li>
+						<p>
+							Check if the link works on my{' '}
+							<ExternalLink
+								to={`https://v14.rileyjshaw.com${pathname}`}
+							>
+								old website
+							</ExternalLink>
+							.
+						</p>
+					</li>
+					<li>
+						<p>
+							Search for{' '}
+							<ExternalLink
+								to={`https://duckduckgo.com/?q=rileyjshaw${pathname.replace(
+									/[/ _-]/g,
+									'+'
+								)}`}
+							>
+								related keywords online
+							</ExternalLink>
+							.
+						</p>
+					</li>
+					<li>
+						<p>
+							Get off the Internet. It's already <Time />.
+						</p>
+					</li>
+				</ul>
+			</main>
+		</>
+	);
+}
 
 export default NotFoundPage;
