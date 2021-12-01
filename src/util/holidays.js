713195e5731bff6b147b7f745634873e723fceca
Riley Shaw <rileyjshaw@gmail.com>
2021-12-01 13:13:44 -0500

    Content: add Black Friday and Cyber Monday banners

--- src/util/holidays.js
+++ src/util/holidays.js
@@ -1,3 +1,8 @@
+function getAmericanThanksgivingDate(year) {
+	const lastDayOfNovember = new Date(year, 10, 30).getDay();
+	return lastDayOfNovember === 6 ? 28 : 27 - lastDayOfNovember;
+}
+
 // Note: the first entry in `date` (month) is 0-indexed. The second (day) is 1-indexed.
 const holidays = [
 	{
@@ -18,20 +23,47 @@ const holidays = [
 			fontWeight: 'bold',
 		},
 	},
+	{
+		name: 'Black Friday',
+		date: year => {
+			return [10, getAmericanThanksgivingDate(year) + 1];
+		},
+		link: 'https://www.monbiot.com/2012/12/10/the-gift-of-death/',
+	},
+	{
+		name: 'Cyber Monday',
+		date: year => {
+			return [10, getAmericanThanksgivingDate(year) + 4];
+		},
+		style: {
+			color: 'var(--color-black)',
+			backgroundColor: 'var(--color-yellow)',
+		},
+		link: 'https://www.monbiot.com/2012/12/10/the-gift-of-death/',
+	},
 ];
 
 export function getNextHoliday() {
 	const today = new Date();
 	const currentYear = today.getFullYear();
+	const nextYear = currentYear + 1;
 	return holidays
 		.map(holiday => {
-			const [month, day] = holiday.date;
+			let month, day;
+			if (typeof holiday.date === 'function') {
+				[month, day] = holiday.date(currentYear);
+			} else {
+				[month, day] = holiday.date;
+			}
 
 			// Add a day in case the holiday is today; we want to ensure it
 			// still looks like it’s in the future.
 			let msUntil = new Date(currentYear, month, day + 1) - today;
 			if (msUntil < 0) {
-				msUntil = new Date(currentYear + 1, month, day + 1) - today;
+				if (typeof holiday.date === 'function') {
+					[month, day] = holiday.date(nextYear);
+				}
+				msUntil = new Date(nextYear, month, day + 1) - today;
 			}
 
 			return {
