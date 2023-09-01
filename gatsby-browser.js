5d52f26f3d1db95a2690ad939ece602f99cbddf8
Riley Shaw <rileyjshaw@gmail.com>
2023-09-01 14:56:31 -0400

    Tooling: Extend Prism language support

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,18 @@
 import AutoLink from './src/components/AutoLink';
 import CodeBlock from './src/components/CodeBlock';
 import Layout from './src/components/Layout';
+import {Prism} from 'prism-react-renderer';
+import React from 'react';
+import bashLang from 'refractor/lang/bash';
+import lispLang from 'refractor/lang/lisp';
+import pythonLang from 'refractor/lang/python';
 import SettingsProvider from './src/components/SettingsProvider';
 import './src/global.css';
 import {MDXProvider} from '@mdx-js/react';
-import React from 'react';
+
+bashLang(Prism);
+lispLang(Prism);
+pythonLang(Prism);
 
 const mdxComponents = {
 	a: AutoLink,
